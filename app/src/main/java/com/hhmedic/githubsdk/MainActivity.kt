565a1ee8b0b2d938c7ba62f982fb03ab0873cae6
lizhichen <lizhichen@hh-medic.com>
2024-03-04 14:37:12 +0800

    first commit

---
+++ app/src/main/java/com/hhmedic/githubsdk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hhmedic.githubsdk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.hhmedic.android.sdk.HHDoctor
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
